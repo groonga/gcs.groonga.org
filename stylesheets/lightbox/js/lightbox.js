45c36e8ddfe9f1dcd972db2dd8c8104e06e5d4eb
Yoji SHIDARA <dara@shidara.net>
2012-07-04 12:50:00 +0900

    Fix paths to images

--- stylesheets/lightbox/js/lightbox.js
+++ stylesheets/lightbox/js/lightbox.js
@@ -48,8 +48,8 @@ lightbox = new Lightbox options
   LightboxOptions = (function() {
 
     function LightboxOptions() {
-      this.fileLoadingImage = 'images/loading.gif';
-      this.fileCloseImage = 'images/close.png';
+      this.fileLoadingImage = '/stylesheets/lightbox/images/loading.gif';
+      this.fileCloseImage = '/stylesheets/lightbox/images/close.png';
       this.resizeDuration = 700;
       this.fadeDuration = 500;
       this.labelImage = "Image";
